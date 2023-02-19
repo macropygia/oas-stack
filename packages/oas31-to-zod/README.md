caa5a4793f809c4491a42532a1e0f789dfdcb95a
macropygia <3162324+macropygia@users.noreply.github.com>
2023-02-19 15:42:48 +0900

    Bump dependencies

--- packages/oas31-to-zod/README.md
+++
@@ -1,5 +0,0 @@
-# oas31-to-zod
-
-Generate Zod schemas from OpenAPI Specification 3.1 Components Object.
-
-- Not compatible with OpenAPI Specification 3.0.
--- packages/playground/astro.config.mjs
+++ packages/playground/astro.config.mjs
@@ -2,13 +2,9 @@ import preact from '@astrojs/preact'
 import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 
-const adapter = vercel({
-  includeFiles: ['api'],
-})
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact({ compat: true })],
   output: 'server',
-  adapter,
+  adapter: vercel(),
 })
